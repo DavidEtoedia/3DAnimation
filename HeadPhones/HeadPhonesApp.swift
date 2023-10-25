b40c80ce1be40db063c0db2c7a3ef95882b509a6
DavidEtoedia <etoedia@gmail.com>
2023-10-25 12:00:56 -0700

    Initial Commit

---
+++ HeadPhones/HeadPhonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadPhonesApp.swift
+//  HeadPhones
+//
+//  Created by Inyene Etoedia on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct HeadPhonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
